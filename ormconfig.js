3699f6d6d3b0b85a0b36649ed2d28540911493ec
Jean Ferreira <jeansouza@pm.me>
2022-01-04 16:28:48 -0300

    Add ormconfig.js

---
+++ ormconfig.js
@@ -0,0 +1,10 @@
+module.exports = {
+  type: "postgres",
+  url: process.env.DATABASE_URL,
+  entities: process.env.TYPEORM_ENTITIES,
+  migrations: process.env.TYPEORM_MIGRATIONS,
+  cli: {
+    migrationDir: process.env.TYPEORM_MIGRATIONS_DIR,
+    entitiesDir: process.env.TYPEORM_ENTITIES_DIR,
+  },
+};
