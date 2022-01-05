95bc49f5145d9d563577b46e08ebbc520b545584
Jean Ferreira <jeansouza@pm.me>
2022-01-04 22:36:58 -0300

    test with array

--- ormconfig.js
+++ ormconfig.js
@@ -3,8 +3,8 @@ console.log(process.env.TYPEORM_ENTITIES);
 module.exports = {
   type: "postgres",
   url: process.env.DATABASE_URL,
-  entities: "dist/app/models/*.js",
-  migrations: "dist/database/migrations/*.js",
+  entities: ["dist/app/models/*.js"],
+  migrations: ["dist/database/migrations/*.js"],
   cli: {
     migrationDir: ["src/database/migrations"],
     entitiesDir: "src/app/models/",
