541002c3f96283048efabdb20ed2186ba03ae814
Jean Ferreira <jeansouza@pm.me>
2022-01-05 16:05:01 -0300

    Add SwaggerUI

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import "reflect-metadata";
 import express from "express";
+import swaggerUi from "swagger-ui-express";
+import swaggerFile from "./swagger/swagger.json";
 
 import "./database/connect";
 import routes from "./routes";
@@ -8,6 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(routes);
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.listen(process.env.PORT || 3000, () =>
   console.log("🔥 Server is running at http://localhost:3000")
