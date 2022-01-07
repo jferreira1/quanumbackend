348a5176c2cec401f1f0caf09e1c86236e2a51e5
Jean Ferreira <jeansouza@pm.me>
2022-01-06 23:09:07 -0300

    Redefines enviroment variables

---
+++ src/database/migrations/1641521212443-CreateAuditsTable.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateAuditsTable1641521212443 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
