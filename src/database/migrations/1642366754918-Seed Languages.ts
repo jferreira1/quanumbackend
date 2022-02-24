c2a1979a5f213b72acc045dd964e5092dbaaebdd
Jean Ferreira <jeansouzza@gmail.com>
2022-02-23 21:54:50 -0300

    create schema migration

---
+++ src/database/migrations/1642366754918-Seed Languages.ts
@@ -0,0 +1,13 @@
+// import { getRepository, MigrationInterface, QueryRunner } from "typeorm";
+// import { Language } from "../../app/entities/Language";
+// import { LanguageSeed } from "../seeds/language.seed";
+
+// export class SeedLanguages1642366754918 implements MigrationInterface {
+//   public async up(queryRunner: QueryRunner): Promise<void> {
+//     await getRepository(Language).save(LanguageSeed);
+//   }
+
+//   public async down(queryRunner: QueryRunner): Promise<void> {
+//     // do nothing
+//   }
+// }
