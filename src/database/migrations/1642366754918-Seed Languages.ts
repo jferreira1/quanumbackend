f8284a7072460f2deb7596388af4c219d0e60053
Jean Ferreira <jeansouzza@gmail.com>
2022-02-23 22:09:05 -0300

    .

--- src/database/migrations/1642366754918-Seed Languages.ts
+++ src/database/migrations/1642366754918-Seed Languages.ts
@@ -1,10 +1,9 @@
 import { getRepository, MigrationInterface, QueryRunner } from "typeorm";
-import { Language } from "../../app/entities/Language";
 import { LanguageSeed } from "../seeds/language.seed";
 
 export class SeedLanguages1642366754918 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await getRepository(Language).save(LanguageSeed);
+    await getRepository("languages").save(LanguageSeed);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
