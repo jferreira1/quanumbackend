5d17a632316c7f9ba76b5e1069760b9b668751f9
Jean Ferreira <jeansouzza@gmail.com>
2022-02-25 17:17:52 -0300

    Add cors to handle headers

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,19 @@ import "reflect-metadata";
 import express from "express";
 import swaggerUi from "swagger-ui-express";
 import swaggerFile from "./swagger/swagger.json";
+import cors from "cors";
 
 import "./database/connect";
 import routes from "./routes";
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "https://quanum-backend.herokuapp.com",
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(routes);
 app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerFile));
