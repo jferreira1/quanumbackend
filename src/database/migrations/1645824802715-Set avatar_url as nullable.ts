6a0c2f389f21b76f0f6fd1265a5e63b7c0f3dad2
Jean Ferreira <jeansouzza@gmail.com>
2022-03-17 16:57:15 -0300

    temp

--- src/database/migrations/1645824802715-Set avatar_url as nullable.ts
+++ src/database/migrations/1645824802715-Set avatar_url as nullable.ts
@@ -1,14 +1,14 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+// import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class SetAvatarUrlAsNullable1645824802715 implements MigrationInterface {
-    name = 'SetAvatarUrlAsNullable1645824802715'
+// export class SetAvatarUrlAsNullable1645824802715 implements MigrationInterface {
+//     name = 'SetAvatarUrlAsNullable1645824802715'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "avatar_url" DROP NOT NULL`);
-    }
+//     public async up(queryRunner: QueryRunner): Promise<void> {
+//         await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "avatar_url" DROP NOT NULL`);
+//     }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "avatar_url" SET NOT NULL`);
-    }
+//     public async down(queryRunner: QueryRunner): Promise<void> {
+//         await queryRunner.query(`ALTER TABLE "users" ALTER COLUMN "avatar_url" SET NOT NULL`);
+//     }
 
-}
+// }
